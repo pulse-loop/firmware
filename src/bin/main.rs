010bbcdc584da238bc6a6963d0fde298912ac7f7
Riccardo Persello <riccardo.persello+i@icloud.com>
2022-10-20 11:37:14 +0200

    Removed BLE from this crate.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -15,6 +15,8 @@
 
 extern crate core;
 
+use esp_idf_hal::can::config::Filter;
+use std::borrow::Borrow;
 use std::{thread, time::Duration};
 
 // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
@@ -25,7 +27,7 @@ mod bluetooth;
 fn main() {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
-    // esp_idf_sys::link_patches();
+    esp_idf_sys::link_patches();
 
     // Initialise logger.
     esp_idf_svc::log::EspLogger::initialize_default();

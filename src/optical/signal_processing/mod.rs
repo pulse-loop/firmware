05a7a9487e392b55961dcf34f1122d28220e17f5
Fabio <fbcragnolini@gmail.com>
2023-01-12 17:13:49 +0100

    Added processing history struct.

--- src/optical/signal_processing/mod.rs
+++ src/optical/signal_processing/mod.rs
@@ -1,3 +1,7 @@
+use queues::{CircularBuffer, IsQueue};
+
+mod histogram;
+
 pub(crate) enum CriticalValue {
     None,
     Minimum,
@@ -12,4 +16,50 @@ impl CriticalValue {
             CriticalValue::Maximum => 0b00000010,
         }
     }
+}
+
+pub(crate) struct ProcessingHistory {
+    /// A sized queue of the last `window_size` elements.
+    window: CircularBuffer<f32>,
+    window_size: usize,
+    /// A histogram of the last `window_size` elements.
+    distribution: histogram::Histogram,
+    /// The last element added to the window.
+    previous_element: Option<f32>,
+    /// The second last element added to the window.
+    previous_previous_element: Option<f32>,
+}
+
+impl ProcessingHistory {
+    pub(crate) fn new(window_size: usize) -> ProcessingHistory {
+        ProcessingHistory {
+            window: CircularBuffer::new(window_size),
+            window_size,
+            distribution: histogram::Histogram::new(1024, -1.2, 1.2),
+            previous_element: None,
+            previous_previous_element: None,
+        }
+    }
+
+    /// Update the window and the distribution of the processing history.
+    pub(crate) fn update(&mut self, el: f32) {
+        let old_el = self.window.add(el).unwrap(); // Add never fails on CircularBuffer.
+
+        self.distribution.increment(el);
+
+        if let Some(old_el) = old_el {
+            self.distribution.decrement(old_el);
+        }
+    }
+}
+
+pub(crate) fn find_critical_value(el: f32, history: &mut ProcessingHistory) -> CriticalValue {
+    // TODO: Finish.
+    if let Some(previous_el) = history.previous_element {
+    } else {
+        // The window was empty.
+        history.previous_element = Some(el);
+    }
+
+    CriticalValue::None
 }
